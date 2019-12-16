059277bed81bb51f2f0832c47687761b4c793958
shiguangwei <shigw5420@joyowo.com>
2019-12-16 23:45:57 +0800

    project init and custom theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +7,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
       </header>
     </div>
   );
---
+++ src/admin.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+
+export default class Admin extends Component {
+
+}
